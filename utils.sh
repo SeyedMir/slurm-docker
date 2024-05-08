c817c6d42bb5be1ae1e6d13021245f14a287204b
Hessam Mirsadeghi <hmirsadeghi@nvidia.com>
2024-05-08 08:58:03 -0700

    Add srun_common util function
    Also pass ntasks-per-node to run_container_node.

--- utils.sh
+++ utils.sh
@@ -7,14 +7,27 @@ print_run() {
     "$@"
 }
 
-run_container_node() {
+srun_common() {
     local container=$1
-    local node=$2
-    shift 2
+    shift 1
+
     if [ -n "${CONTAINER_MOUNTS:-}" ]; then
         set -- --container-mounts "$CONTAINER_MOUNTS" "$@"
     fi
-    set -- srun --mpi=pmix -N 1 --ntasks-per-node 1 -w "$node" \
-        --container-name "$container" --overlap "$@"
+
+    set -- srun --mpi=pmix --overlap --container-name "$container" "$@"
     print_run "$@"
+
+}
+
+# Run the command line in a given container on a given node
+# with a given number of tasks on that node.
+run_container_node() {
+    local container=$1
+    local node=$2
+    local ntasks=$3
+    shift 3
+
+    set -- -N 1 --ntasks-per-node "$ntasks" -w "$node" "$@"
+    srun_common "$container" "$@"
 }
