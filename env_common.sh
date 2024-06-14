b052bd3466fc501999e1337270f97533e4c3f34d
Hessam Mirsadeghi <hmirsadeghi@nvidia.com>
2024-06-14 14:55:17 -0700

    Move CONTAINER_MOUNTS to env_common.sh

--- env_common.sh
+++ env_common.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 RESULTS_DIR=${RESULTS_DIR:-${PWD}/results}
 [ -d "$RESULTS_DIR" ] || mkdir -p "$RESULTS_DIR"
 
+export CONTAINER_MOUNTS="$PWD/container_scripts/:/container_scripts,${RESULTS_DIR}:/results"
+
 export UCX_PROTO_INFO=${UCX_PROTO_INFO:=y}
 export UCX_LOG_LEVEL=${UCX_LOG_LEVEL:=info}
 export UCX_CUDA_COPY_DMABUF=no
